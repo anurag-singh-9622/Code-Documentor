a30e2e9c78e55f4b70472b43d4b1b306dfe151b5
Anurag Singh <85456164+anurag-singh-9622@users.noreply.github.com>
2024-04-13 18:38:07 +0530

    added new files

---
+++ main.py
@@ -0,0 +1,93 @@
+from openai import OpenAI
+import streamlit as st
+import random, json
+
+client = OpenAI(api_key = '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')
+# with open('doc.json') as file:
+#     # Step 2: Parse the JSON data
+#     data = json.load(file)
+if 'text' not in st.session_state:
+    st.session_state.text = False
+
+def text_input():
+    st.session_state.text = True
+
+if 'clicked' not in st.session_state:
+    st.session_state.clicked = False
+
+def click_button():
+    st.session_state.clicked = True
+
+if 'selected' not in st.session_state:
+    st.session_state.selected = False
+
+def selected():
+    st.session_state.selected = True
+###########################################################################################
+#Adding gali sentences
+names = ['Vaibhav', 'Deepu', 'Shantanu', 'Ankit', 'Rahul', 'Apoorv', 'Riya']
+galis = ['bhenchod', 'bosdiwale', 'madharchod', 'gandu', 'ben ka loda', 'chutia', 'jiska muh lode jaisa']
+
+# Define a format function
+def format_func(option):
+    return option
+ 
+if not st.session_state.clicked:
+    st.session_state.gali = random.choice (galis)
+
+# Create the select box with custom formatting
+selected_option = st.selectbox('Choose an option:', names, format_func=format_func, index=None, placeholder = "Choose an option", on_change=selected)
+
+if st.session_state.selected:
+    # Display the selected option
+    st.write('Chal code likh', f":red[{selected_option} {st.session_state.gali}]")
+
+###########################################################################################
+
+
+
+
+
+prompt = st.text_area(label="Code input",placeholder="Write your code here",height=305, on_change=text_input)
+
+def llm(promt):
+    response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {
+        "role": "system",
+        "content": "You are a code documentation assistant//\n which helps creating the document for the code for developers.// \n//You will create the document in Markdown format."
+        },
+        {
+        "role": "user",
+        "content": f"""Elaborate all the function, loops, if/else, variables or anything else that might be important and what is its use which you are using in the code.
+         code: ```{prompt}```"""
+        }
+    ],
+
+    temperature=0.8,
+    max_tokens=2000,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+ 
+    return response
+
+
+if st.session_state.text and len(prompt) > 1:
+    st.button('Submit', on_click=click_button)
+    if st.session_state.clicked:
+        response = llm(prompt)
+
+        with st.sidebar:
+            f"Total tokens:   {response.usage.total_tokens}"
+        result = response.choices[0].message.content
+        st.success(f'Ye le {selected_option} {random.choice (galis)}')
+        result
+        print(result)
+elif st.session_state.text:
+    if selected_option is not None: #you can remove this line if not needed
+        st.warning('Pura code likh '+ f":red[{selected_option} {random.choice (galis)}]")
+
+
